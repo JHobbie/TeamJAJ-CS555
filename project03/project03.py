f9444dabb2b362a616edb5ad4653ca12ab892dfc
JHobbie <jaredhobbie1@gmail.com>
2019-09-14 14:49:51 -0400

    Changed Format of currentObject

--- project03/project03.py
+++ project03/project03.py
@@ -11,6 +11,7 @@ tags1 = ["NAME", "SEX", "BIRT", "DEAT", "FAMC",
     "FAMS", "MARR", "HUSB", "WIFE", "CHIL", "DIV"]
 tags2 = ["DATE"]
 other_tags = ["INDI", "FAM"]
+dateTags = ["BIRT", "DEAT", "MARR",  "DIV"]
 familyDict = {}
 individualDict = {}
 dictDict = {"FAM": familyDict, "INDI": individualDict}
@@ -28,7 +29,7 @@ if (len(fileName) > 0):
         outputFileName += ".txt"
     loadedFile = open(fileName, 'r')
     writtenFile = open(outputFileName, 'a')
-    currentObject = []
+    currentObject = {}
     for i in loadedFile:
         oldLine = "-->" + i.strip() + "\n"
         lineArr = i.split()
@@ -40,21 +41,25 @@ if (len(fileName) > 0):
             constructedLine = "<--" + str(lev) + "|"+tag+"|"+"N"+"|"+arg.strip() + "\n"
         elif (lev == 1 and tag in tags1) or (lev == 2 and tag in tags2):
            constructedLine = "<--"+str(lev)+"|" + tag+"|"+"Y"+"|"+arg.strip() + "\n"
-           tup = (tag, arg)
-           currentObject.append(tup)
+           if tag in dateTags:
+               lastDate = tag
+           if tag == "DATE":
+                currentObject[lastDate] = arg
+           else: 
+               currentObject[tag] = arg
         elif lev == 0 and tag in tags0:
             constructedLine = "<--"+str(lev)+"|" + tag+"|"+"Y"+"|"+arg.strip() + "\n"
-            if len(currentObject) >= 2:
-               dictDict[currentObject[0]][currentObject[1]] = currentObject
-            currentObject = []
+            if currentObject != {}:
+               dictDict[currentObject["type"]][currentObject["ID"]] = currentObject
+            currentObject = {}
         elif lineArr[-1] in other_tags and lev == 0:
             constructedLine = "<--"+str(lev)+"|"+ str(lineArr[-1]).strip()+"|"+"Y"+"|"+ str(lineArr[1]) + "\n"
-            if len(currentObject)>=2:
-                dictDict[currentObject[0]][currentObject[1]] = currentObject
+            if currentObject != {}:
+                dictDict[currentObject["type"]][currentObject["ID"]] = currentObject
                 writtenFile.write(str(currentObject))
-            currentObject = []
-            currentObject.append(str(lineArr[-1]).strip())
-            currentObject.append(lineArr[1])
+            currentObject = {}
+            currentObject["type"]=str(lineArr[-1]).strip()
+            currentObject["ID"] = lineArr[1]
         else:
             constructedLine = "<--"+str(lev)+ "|"+tag+"|"+"N"+"|"+arg.strip() + "\n"
         print (oldLine, constructedLine)
