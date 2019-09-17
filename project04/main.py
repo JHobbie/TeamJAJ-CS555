021fb7fc653da12cb7286fdd7dd1320f3ca0319f
JHobbie <jaredhobbie1@gmail.com>
2019-09-17 16:38:58 -0400

    Refactored main

--- project04/main.py
+++ project04/main.py
@@ -17,8 +17,7 @@ dateTags = ["BIRT", "DEAT", "MARR",  "DIV"]
 familyDict = {}
 individualDict = {}
 dictDict = {"FAM": familyDict, "INDI": individualDict}
-
-if __name__ == "__main__":
+def loadFile():
     fileName = raw_input("Please enter the desired GEDCOM file name: ")
     fileName = fileName.strip()
     outputFileName = "parsedOutput" + fileName
@@ -31,101 +30,121 @@ if __name__ == "__main__":
         else:
             fileName += ".ged"
             outputFileName += ".txt"
-        loadedFile = open(fileName, 'r')
-        writtenFile = open(outputFileName, 'a')
-        currentObject = {}
-        for i in loadedFile: #TODO: Refactor at some point (IE Probably Never)
-            oldLine = "-->" + i.strip() + "\n"
-            lineArr = i.split()
-            lev = int(lineArr[0])
-            tag = lineArr[1]
-            arg = ' '.join(lineArr[2:])
-            constructedLine = ""
-            if lev > 2:
-                constructedLine = "<--" + \
-                    str(lev) + "|"+tag+"|"+"N"+"|"+arg.strip() + "\n"
-            elif (lev == 1 and tag in tags1) or (lev == 2 and tag in tags2):
-                constructedLine = "<--"+str(lev)+"|" + \
-                    tag+"|"+"Y"+"|"+arg.strip() + "\n"
-                if tag in dateTags:
-                    lastDate = tag
-                if tag == "DATE":
-                    currentObject[lastDate] = [arg]
+        return (fileName, outputFileName)
+    else:
+        print("You have entered an invalid file name!")
+        raise ValueError
+
+def writeFileAndFillDict(inputFileName, outputFileName):
+    loadedFile = open(inputFileName, 'r')
+    writtenFile = open(outputFileName, 'a')
+    currentObject = {}
+    for i in loadedFile: 
+        oldLine = "-->" + i.strip() + "\n"
+        lineArr = i.split()
+        lev = int(lineArr[0])
+        tag = lineArr[1]
+        arg = ' '.join(lineArr[2:])
+        constructedLine = ""
+        if lev > 2:
+            constructedLine = "<--" + \
+                str(lev) + "|"+tag+"|"+"N"+"|"+arg.strip() + "\n"
+        elif (lev == 1 and tag in tags1) or (lev == 2 and tag in tags2):
+            constructedLine = "<--"+str(lev)+"|" + \
+                tag+"|"+"Y"+"|"+arg.strip() + "\n"
+            if tag in dateTags:
+                lastDate = tag
+            if tag == "DATE":
+                currentObject[lastDate] = [arg]
+            else:
+                if tag in currentObject:
+                    currentObject[tag] = currentObject[tag]+[arg]
                 else:
-                    if tag in currentObject:
-                        currentObject[tag] = currentObject[tag]+[arg]
-                    else:
-                        currentObject[tag] = [arg]
-            elif lev == 0 and tag in tags0:
-                constructedLine = "<--"+str(lev)+"|" + \
-                    tag+"|"+"Y"+"|"+arg.strip() + "\n"
-                if currentObject != {}:
-                    dictDict[currentObject["type"]
-                             ][currentObject["ID"]] = currentObject
-                currentObject = {}
-            elif lineArr[-1] in other_tags and lev == 0:
-                constructedLine = "<--" + \
-                    str(lev)+"|" + str(lineArr[-1]).strip() + \
-                    "|"+"Y"+"|" + str(lineArr[1]) + "\n"
-                if currentObject != {}:
-                    dictDict[currentObject["type"]
-                             ][currentObject["ID"]] = currentObject
-                currentObject = {}
-                currentObject["type"] = str(lineArr[-1]).strip()
-                currentObject["ID"] = lineArr[1]
-            else:
-                constructedLine = "<--" + \
-                    str(lev) + "|"+tag+"|"+"N"+"|"+arg.strip() + "\n"
-            writtenFile.write(oldLine)
-            writtenFile.write(constructedLine)
-        loadedFile.close()
-        writtenFile.close()
-        includeAge.addAgeToAll(individualDict)
-        indiTable = PrettyTable(
-            ['ID', 'Name', 'Gender', 'Birth', 'Death', 'Alive', 'Child', 'Spouse', 'Age'])
-        for key in individualDict:
-            addlist = [key] + individualDict[key]['NAME'] + \
-                individualDict[key]['SEX'] + individualDict[key]['BIRT']
-            if 'DEAT' in individualDict[key].keys():
-                addlist += individualDict[key]['DEAT']
-                addlist.append('N')
-            else:
-                addlist.append('N/A')
-                addlist.append('Y')
-            if 'FAMC' in individualDict[key].keys():
-                addlist += individualDict[key]['FAMC']
-            else:
-                addlist.append('N/A')
-            if 'FAMS' in individualDict[key].keys():
-                addlist += individualDict[key]['FAMS']
-            else:
-                addlist.append('N/A')
-            addlist.append(individualDict[key]['AGE'])
-            indiTable.add_row(addlist)
-        writefi = open('printoutput.txt', 'a')
-        writefi.write('Individuals:\n')
-        writefi.write(indiTable.get_string() + '\n')
-        writefi.write('Families:\n')
-        famTable = PrettyTable(['ID', 'Married', 'Divorced', 'Husband ID',
-                                'Husband Name', 'Wife ID', 'Wife Name', 'Children'])
-        for key in familyDict:
-            addlist = [key] + familyDict[key]['MARR']
-            if 'DIV' in familyDict.keys():
-                addlist += familyDict[key]['DIV']
-            else:
-                addlist.append('N/A')
-            hid = familyDict[key]['HUSB'][0]
-            wid = familyDict[key]['WIFE'][0]
-            addlist += [hid] + individualDict[hid]['NAME'] + \
-                [wid] + individualDict[wid]['NAME']
-            if 'CHIL' in familyDict[key].keys():
-                addlist += [familyDict[key]['CHIL']]
-            else:
-                addlist.append('N/A')
-            famTable.add_row(addlist)
-        writefi.write(famTable.get_string() + "\n")
-        us10Anomalies = marriageAge.detectPedophilia(familyDict, individualDict)
-        utils.writeErrors(us10Anomalies, writefi)
-        writefi.close()
+                    currentObject[tag] = [arg]
+        elif lev == 0 and tag in tags0:
+            constructedLine = "<--"+str(lev)+"|" + \
+                tag+"|"+"Y"+"|"+arg.strip() + "\n"
+            if currentObject != {}:
+                dictDict[currentObject["type"]
+                            ][currentObject["ID"]] = currentObject
+            currentObject = {}
+        elif lineArr[-1] in other_tags and lev == 0:
+            constructedLine = "<--" + \
+                str(lev)+"|" + str(lineArr[-1]).strip() + \
+                "|"+"Y"+"|" + str(lineArr[1]) + "\n"
+            if currentObject != {}:
+                dictDict[currentObject["type"]
+                            ][currentObject["ID"]] = currentObject
+            currentObject = {}
+            currentObject["type"] = str(lineArr[-1]).strip()
+            currentObject["ID"] = lineArr[1]
+        else:
+            constructedLine = "<--" + \
+                str(lev) + "|"+tag+"|"+"N"+"|"+arg.strip() + "\n"
+        writtenFile.write(oldLine)
+        writtenFile.write(constructedLine)
+    loadedFile.close()
+    writtenFile.close()
+    return
+
+def constructAndWriteIndiTable(file):
+    file.write('Individuals:\n')
+    indiTable = PrettyTable(
+        ['ID', 'Name', 'Gender', 'Birth', 'Death', 'Alive', 'Child', 'Spouse', 'Age'])
+    for key in individualDict:
+        addlist = [key] + individualDict[key]['NAME'] + \
+            individualDict[key]['SEX'] + individualDict[key]['BIRT']
+        if 'DEAT' in individualDict[key].keys():
+            addlist += individualDict[key]['DEAT']
+            addlist.append('N')
+        else:
+            addlist.append('N/A')
+            addlist.append('Y')
+        if 'FAMC' in individualDict[key].keys():
+            addlist += individualDict[key]['FAMC']
+        else:
+            addlist.append('N/A')
+        if 'FAMS' in individualDict[key].keys():
+            addlist += individualDict[key]['FAMS']
+        else:
+            addlist.append('N/A')
+        addlist.append(individualDict[key]['AGE'])
+        indiTable.add_row(addlist)
+    file.write(indiTable.get_string() + '\n')
+    return
+
+def constructAndWriteFamTable(writefi):
+    writefi.write('Families:\n')
+    famTable = PrettyTable(['ID', 'Married', 'Divorced', 'Husband ID',
+                            'Husband Name', 'Wife ID', 'Wife Name', 'Children'])
+    for key in familyDict:
+        addlist = [key] + familyDict[key]['MARR']
+        if 'DIV' in familyDict.keys():
+            addlist += familyDict[key]['DIV']
+        else:
+            addlist.append('N/A')
+        hid = familyDict[key]['HUSB'][0]
+        wid = familyDict[key]['WIFE'][0]
+        addlist += [hid] + individualDict[hid]['NAME'] + \
+            [wid] + individualDict[wid]['NAME']
+        if 'CHIL' in familyDict[key].keys():
+            addlist += [familyDict[key]['CHIL']]
+        else:
+            addlist.append('N/A')
+        famTable.add_row(addlist)
+    writefi.write(famTable.get_string() + "\n")
+
+if __name__ == "__main__":
+    fileName, outputFileName = loadFile()
+    writeFileAndFillDict(fileName, outputFileName)
     
+    includeAge.addAgeToAll(individualDict)
+    writefi = open('printoutput.txt', 'a')
+    constructAndWriteIndiTable(writefi)
+    constructAndWriteFamTable(writefi)
+    
+    us10Anomalies = marriageAge.detectPedophilia(familyDict, individualDict)
+    utils.writeErrors(us10Anomalies, writefi)
+    writefi.close()
+
 
