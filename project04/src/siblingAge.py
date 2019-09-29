1973a18c779c5c27e64b5a2b21b0c8436a1e679f
Jo Ramaswamy <41977400+j-swamy@users.noreply.github.com>
2019-09-29 15:28:48 -0400

    modified to return a list

--- project04/src/siblingAge.py
+++ project04/src/siblingAge.py
@@ -1,8 +1,9 @@
-def siblingAge(familyDict, individualDict, file):
-    file.write('US28 Ordering Siblings by age:\n')
+def siblingAge(familyDict, individualDict):
+    retList = []
+    #file.write('US28 Ordering Siblings by age:\n')
     for key in familyDict.keys():
         currentFam = familyDict[key]
-        file.write("Family id %s \n" %  (currentFam['ID']))
+        #file.write("Family id %s \n" %  (currentFam['ID']))
         if( "CHIL" in currentFam):
             kidList = currentFam["CHIL"]
             newKidList = []
@@ -11,10 +12,11 @@ def siblingAge(familyDict, individualDict, file):
                 newKidList.append(currKid)
             newKidList.sort(key = lambda child: child['AGE'])
             newKidList.reverse()
-            for kid in newKidList:
-                file.write("%s: %s is %d years old\n" %(kid['ID'], kid['NAME'][0], kid['AGE']))
+            #for kid in newKidList:
+                #file.write("%s: %s is %d years old\n" %(kid['ID'], kid['NAME'][0], kid['AGE']))
+            retList += newKidList
         else:
-            file.write("No children in this family\n")
-    return
+            #file.write("No children in this family\n"
+            retList += ["No children in this family"]
+    return retList
 
-            
