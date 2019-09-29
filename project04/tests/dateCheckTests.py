d8a1032bacea67d175a5ecdf53d65b0d66d4c2d0
Jo Ramaswamy <41977400+j-swamy@users.noreply.github.com>
2019-09-29 15:27:12 -0400

    added deep's tests

---
+++ project04/tests/dateCheckTests.py
@@ -0,0 +1,37 @@
+import unittest
+import project04.src.dateCheck as dateCheck
+
+def suite():
+    suite = unittest.TestLoader().loadTestsFromTestCase(illegitimateDatesTestCase)
+    return suite
+class dateCheckTestCase(unittest.TestCase):
+    def setUp(self):
+        #i02
+        self.badDate = {'NAME': ['Ginger /Bred/'],  'FAMS': ['F01'], 'BIRT': ['10 AUG 1989'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I02', 'DEAT': ['9 OCT 2020']}
+        #i01
+        self.goodDate = {'NAME': ['Hersh E. /Bar/'],  'FAMS': ['F01'], 'BIRT': ['21 FEB 1970'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I01'}
+        #i03
+        self.badDate2 = {'NAME': ['Candy /Kane/'],  'FAMS': ['F02'], 'BIRT': ['20 DEC 2020'], 'SEX': ['F'], 'type': 'INDI', 'ID': 'I03'}
+        #i04
+        self.goodDate2 = {'NAME': ['George /Kane/'],  'FAMS': ['F02'], 'BIRT': ['29 FEB 1992'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I04'}
+
+        self.familyBad = {'MARR': ['19 FEB 2021'], 'WIFE': ['I02'], 'CHIL': ['I03', 'I05', 'I06', 'I07'], 'HUSB': ['I01'], 'type': 'FAM', 'ID': 'F01'}
+        self.familyGood = {'MARR': ['14 FEB 2000'], 'WIFE': ['I03'], 'CHIL': [ 'I05', 'I06', 'I07'], 'HUSB': ['I04'], 'type': 'FAM', 'ID': 'F02'}
+        
+        self.familyDict = {'F01': self.familyBad, 'F02' : self.familyGood}
+        self.individualDict = {'I02' : self.badDate, 'I01' : self.goodDate, 'I03' : self.badDate2, 'I04' : self.goodDate2}
+
+    
+    def tearDown(self):
+        self.badDate = None
+        self.badDate2 = None
+        self.goodDate = None
+        self.goodDate2 = None
+    
+    
+    def test_dateCheck(self):
+        err_msg = dateCheck.dateCheck(self.familyDict, self.individualDict)
+        self.assertEqual(3, len(err_msg), "Individual I02 in Family F01 has an invalid death date, Individual I03 in Family F01 has an invalid birth date, Family F01 has an invalid marriage date")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ project04/tests/siblingAgeTests.py
@@ -0,0 +1,43 @@
+import unittest
+import project04.src.siblingAge as siblingAge
+
+def suite():
+    suite = unittest.TestLoader().loadTestsFromTestCase(illegitimateDatesTestCase)
+    return suite
+class siblingAgeTestCase(unittest.TestCase):
+    def setUp(self):
+        #i02
+        self.mom = {'NAME': ['Ginger /Bred/'],  'FAMS': ['F01'], 'BIRT': ['10 AUG 1989'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I02', 'DEAT': ['9 OCT 2020']}
+        #i01
+        self.dad = {'NAME': ['Hersh E. /Bar/'],  'FAMS': ['F01'], 'BIRT': ['21 FEB 1970'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I01'}
+        #i03
+        self.child1 = {'NAME': ['Candy /Kane/'],  'FAMS': ['F02'], 'BIRT': ['20 DEC 2020'], 'SEX': ['F'], 'type': 'INDI', 'ID': 'I03'}
+        #i05
+        self.child2 = {'NAME': ['Reese /Bar/'], 'BIRT': ['20 MAY 1994'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I04'}
+        #i07
+        self.child3 = {'NAME': ['Lemon /Drop/'],  'FAMS': ['F03'], 'BIRT': ['11 JUN 1995'], 'SEX': ['F'], 'type': 'INDI', 'ID': 'I07'}
+        #i06
+        self.child4 = {'NAME': ['Mars /Bar/'], 'BIRT': ['20 MAY 1994'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I06'}
+
+        self.family = {'MARR': ['19 FEB 2021'], 'WIFE': ['I02'], 'CHIL': ['I03', 'I05', 'I06', 'I07'], 'HUSB': ['I01'], 'type': 'FAM', 'ID': 'F01'}
+        
+        self.familyDict = {'F01': self.family}
+        self.individualDict = {'I02' : self.mom, 'I01' : self.dad, 'I03' : self.child1, 'I05' : self.child2, 'I06' : self.child4, 'I07' : self.child3}
+
+    
+    def tearDown(self):
+        self.mom = None
+        self.dad = None
+        self.child1 = None
+        self.child2 = None
+        self.child3 = None
+        self.child4 = None
+    
+    
+    def test_siblingAge(self):
+        siblingList = siblingAge.siblingAge(self.familyDict, self.individualDict)
+        self.assertEqual(4, len(siblingList), "4 siblings in the family, ordered Reese, Mars, Candy, Lemon.")
+
+
+if __name__ == '__main__':
+    unittest.main()
