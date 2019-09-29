6d6559828a994e4f1dc8118d703cadc68c2a4231
Jo Ramaswamy <41977400+j-swamy@users.noreply.github.com>
2019-09-29 15:28:17 -0400

    updated testrunner w new tests

--- project04/tests/testRunner.py
+++ project04/tests/testRunner.py
@@ -2,6 +2,7 @@ import unittest
 import  project04.tests.utilsTests as utilsTests
 import project04.tests.marriageAgeTests as marriageAgeTests, project04.tests.includeAgeTests as includeAgeTests
 import project04.tests.illegitimateAgeTests as illegitimateAgeTests, project04.tests.correctGenderTests as correctGenderTests
+import project04.tests.dateCheckTests as dateCheckTests, project04.tests.siblingAgeTests as siblingAgeTests
 
 if __name__ == '__main__':
     #mainTestSuite = mainTests.suite()
@@ -10,5 +11,7 @@ if __name__ == '__main__':
     includeAgeTestSuite = includeAgeTests.suite()
     correctGenderTestSuite = correctGenderTests.suite()
     illegitimateAgeTestSuite = illegitimateAgeTests.suite()
-    alltests = unittest.TestSuite([utilsTestSuite, marriageAgeTestSuite, includeAgeTestSuite, correctGenderTestSuite, illegitimateAgeTestSuite])
+    dateCheckTestSuite = dateCheckTests.suite()
+    siblingAgeTestSuite = siblingAgeTests.suite()
+    alltests = unittest.TestSuite([utilsTestSuite, marriageAgeTestSuite, includeAgeTestSuite, correctGenderTestSuite, illegitimateAgeTestSuite, dateCheckTestSuite, siblingAgeTestSuite])
     unittest.TextTestRunner(verbosity=2).run(alltests)
