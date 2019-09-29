3de6d54687e7d1b5ad76c97c15de271da455c646
Jo Ramaswamy <41977400+j-swamy@users.noreply.github.com>
2019-09-29 14:38:45 -0400

    fixed us01

--- project04/src/dateCheck.py
+++ project04/src/dateCheck.py
@@ -2,24 +2,31 @@
 import datetime
 
 #US01
+
 def dateCheck(familyDict, individualDict):
-    currDate = datetime.now()
-    dateBool = True
+    err_msg = []
+    currDate = datetime.datetime.today()
     for key in familyDict.keys():
         currFam = familyDict[key]
-        if currFam['MARR'][0] > currDate:
-            dateBool = False
-        if currFam['DIV'][0] != None:
-            if currFam['DIV'][0] > currDate:
-                dateBool = False
-    return dateBool
 
+        calculatedDate = datetime.datetime.strptime(currFam['MARR'][0], '%d %b %Y')
+        if calculatedDate > currDate:
+            err_msg += ["Family " + currFam['ID'] + " has marriage date in the future"]
+        if 'DIV' in currFam.keys():
+            calculatedDate = datetime.datetime.strptime(currFam['DIV'][0], '%d %b %Y')
+            if (calculatedDate > currDate):
+                err_msg += ["Family " + currFam['ID'] + " has divorce date in the future"]
+   
     for key in individualDict.keys():
         currInd = individualDict[key]
-        if currInd['BIRT'][0] > currDate:
-            dateBool = False
-        if currInd['DEAT'][0] != None:
-            if currInd['DEAT'][0] > currDate:
-                dateBool = False
-    return dateBool
         
+        calculatedDate = datetime.datetime.strptime(currInd['BIRT'][0], '%d %b %Y')
+        if (calculatedDate > currDate):
+            err_msg += ["Individual " + currInd['ID'] + " has birth date in the future"]
+        if 'DEAT' in currInd.keys():
+            calculatedDate = datetime.datetime.strptime(currInd['DEAT'][0], '%d %b %Y')
+            if (calculatedDate > currDate):
+                err_msg += ["Individual " + currInd['ID'] + " has death date in the future"]
+
+    return err_msg
+
