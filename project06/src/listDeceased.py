a03e16feb82bb663d2e5a62be7059e58d787d54a
asingh97 <asingh28@stevens.edu>
2019-10-11 11:47:32 -0400

    fixed return for listDeceased and livingMarried

--- project06/src/listDeceased.py
+++ project06/src/listDeceased.py
@@ -3,6 +3,10 @@
 def listDeceased(individualDict):
     for key in individualDict.keys():
         currInd = individualDict[key]
+        retlist = []
         if 'DEAT' in currInd.keys():
-            return currInd['NAME']
+            retlist += [("US29: " + currInd['NAME'] + "is deceased.")]
+        else:
+            retlist += [("US29: There are no deceased individuals.")]
+    return retlist
         
--- project06/src/livingMarried.py
+++ project06/src/livingMarried.py
@@ -3,6 +3,10 @@
 def livingMarried(individualDict):
     for key in individualDict.keys():
         currInd = individualDict[key]
+        retlist = []
         if 'MARR' in currInd.keys():
             if 'DEAT' not in currInd.keys():
-                return currInd['NAME']
+                retlist += [("US30: " + currInd['NAME'] + "is living and married.")]
+        else:
+            retlist += [("There are no living and married individuals.")]
+    return retlist
