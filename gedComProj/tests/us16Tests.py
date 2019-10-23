857920e9a4e05034fccfe2b499e83ced441e6cf0
Jo Ramaswamy <41977400+j-swamy@users.noreply.github.com>
2019-10-23 00:21:12 -0400

    quick update

--- gedComProj/tests/us16Tests.py
+++ gedComProj/tests/us16Tests.py
@@ -4,30 +4,30 @@ import gedComProj.src.us16Story as us16Story
 def suite():
     suite = unittest.TestLoader().loadTestsFromTestCase(us16TestCase)
     return suite
-class us14TestCase(unittest.TestCase):
+class us16TestCase(unittest.TestCase):
     def setUp(self):
         #i02
-		self.mom = {'NAME': ['Ginger /Bred/'],  'FAMS': ['F01'], 'BIRT': ['10 AUG 1989'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I02', 'DEAT': ['9 OCT 2020']}
-		#i01
-		self.dad = {'NAME': ['Hersh E. /Bar/'],  'FAMS': ['F01'], 'BIRT': ['21 FEB 1970'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I01'}
-		#i03
-		self.child1 = {'NAME': ['Candy /Kane/'],  'FAMS': ['F02'], 'BIRT': ['20 MAY 1994'], 'SEX': ['F'], 'type': 'INDI', 'ID': 'I03'}
-		#i05
-		self.child2 = {'NAME': ['Reese /Piece/'], 'BIRT': ['20 MAY 1994'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I05'}
-		#i07
-		self.child3 = {'NAME': ['Lemon /Drop/'],  'FAMS': ['F03'], 'BIRT': ['20 MAY 1994'], 'SEX': ['F'], 'type': 'INDI', 'ID': 'I07'}
-		#i06
-		self.child4 = {'NAME': ['Mars /Bar/'], 'BIRT': ['20 MAY 1994'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I06'}
-		#i08
-		self.child5 = {'NAME': ['Twix /Stick/'], 'BIRT': ['20 MAY 1994'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I08'}
-		#i09
-		self.child6 = {'NAME': ['Kitkat /Bar/'], 'BIRT': ['20 MAY 1994'], 'SEX': ['F'], 'type': 'INDI', 'ID': 'I09'}
-		
-		self.family = {'MARR': ['19 FEB 1990'], 'WIFE': ['I02'], 'CHIL': ['I03', 'I05', 'I06', 'I07', 'I08', 'I09'], 'HUSB': ['I01'], 'type': 'FAM', 'ID': 'F01'}
+	self.mom = {'NAME': ['Ginger /Bred/'],  'FAMS': ['F01'], 'BIRT': ['10 AUG 1989'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I02', 'DEAT': ['9 OCT 2020']}
+	#i01
+	self.dad = {'NAME': ['Hersh E. /Bar/'],  'FAMS': ['F01'], 'BIRT': ['21 FEB 1970'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I01'}
+	#i03
+	self.child1 = {'NAME': ['Candy /Kane/'],  'FAMS': ['F02'], 'BIRT': ['20 MAY 1994'], 'SEX': ['F'], 'type': 'INDI', 'ID': 'I03'}
+	#i05
+	self.child2 = {'NAME': ['Reese /Piece/'], 'BIRT': ['20 MAY 1994'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I05'}
+	#i07
+	self.child3 = {'NAME': ['Lemon /Drop/'],  'FAMS': ['F03'], 'BIRT': ['20 MAY 1994'], 'SEX': ['F'], 'type': 'INDI', 'ID': 'I07'}
+	#i06
+	self.child4 = {'NAME': ['Mars /Bar/'], 'BIRT': ['20 MAY 1994'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I06'}
+	#i08
+	self.child5 = {'NAME': ['Twix /Stick/'], 'BIRT': ['20 MAY 1994'], 'SEX': ['M'], 'type': 'INDI', 'ID': 'I08'}
+	#i09
+	self.child6 = {'NAME': ['Kitkat /Bar/'], 'BIRT': ['20 MAY 1994'], 'SEX': ['F'], 'type': 'INDI', 'ID': 'I09'}
+
+	self.family = {'MARR': ['19 FEB 1990'], 'WIFE': ['I02'], 'CHIL': ['I03', 'I05', 'I06', 'I07', 'I08', 'I09'], 'HUSB': ['I01'], 'type': 'FAM', 'ID': 'F01'}
 
 
-		self.familyDict = {'F01': self.family}
-		self.individualDict = {'I02' : mom, 'I01' : dad, 'I03' : child1, 'I05' : child2, 'I06' : child4, 'I07' : child3, 'I08' : child5, 'I09' : child6}
+	self.familyDict = {'F01': self.family}
+	self.individualDict = {'I02' : self.mom, 'I01' : self.dad, 'I03' : self.child1, 'I05' : self.child2, 'I06' : self.child4, 'I07' : self.child3, 'I08' : self.child5, 'I09' : self.child6}
 
     def tearDown(self):
         self.mom = None
