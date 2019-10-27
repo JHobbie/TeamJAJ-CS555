e822310df69a3745baad36bbcc72399126fbd19e
Jyotsna Ramaswamy <jo.ramaswamy@gmail.com>
2019-10-27 14:35:54 -0400

    fixed some variable errors

--- gedComProj/src/fewerSiblings.py
+++ gedComProj/src/fewerSiblings.py
@@ -1,14 +1,13 @@
 #US15
 
 def fewerSiblings(familyDict):
-    retlist = []
+    retList = []
     for key in familyDict.keys():
         currFam = familyDict[key]
-        retList +=  [("US15: Family id %s" %  (currentFam['ID']))]
         if 'CHIL' in currFam:
             kidlist = currFam['CHIL']
             if len(kidlist) >= 15:
-                retList += [("US15: There are too many kids in this family")]
+                retList += [("US15: There are too many kids in Family " + currFam['ID'])]
         else:
             retList+= ["US15: No children in this family"]
     return retList
