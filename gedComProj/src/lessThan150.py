a23c59b6a7c9b10f8ec354e5fbf4a5cfac69793c
asingh97 <asingh28@stevens.edu>
2019-10-27 15:02:05 -0400

    fixed lessThan150 US07

--- gedComProj/src/lessThan150.py
+++ gedComProj/src/lessThan150.py
@@ -2,18 +2,40 @@ import datetime
 
 #US07
 
+def calcDate(dateString):
+    try:
+        calculatedDate = datetime.datetime.strptime(dateString, '%d %b %Y')
+        return calculatedDate
+    except:
+        print("Could not parse date, returning now")
+        # We get no credit for detecting syntactic errors, but may want to know when 
+        return datetime.datetime.now()
+
+def calculateAge(individual, inputDateObj):
+    birthDate = individual["BIRT"][0]
+    try:
+        birthDateObj = calcDate(birthDate)
+    except:
+        print("Could not parse individual's birthdate")
+        # We get no credit for detecting syntactic errors, but may want to know when 
+        return -1
+    else:
+        ageAtInputDate = inputDateObj.year - birthDateObj.year -  (1 if (inputDateObj.month< birthDateObj.month) else (1 if ((inputDateObj.month == birthDateObj.month) and (inputDateObj.day < birthDateObj.day)) else 0))
+        return ageAtInputDate
+
 def lessThan150(individualDict):
     retList = []
     currDate = datetime.now()
     for key in individualDict.keys():
         currInd = individualDict[keys]
+        deathDate = calcDate(currInd['DEAT'][0])
+        birthDate = calcDate(currInd['BIRT'][0])
         if 'DEAT' in currInd.keys():
-            deathAge = currInd['DEAT'] - currInd['BIRT']
-            if deathAge >= 150:
-                retList += [("US07: " + currInd['NAME'] + "was too old when they passed away.")]
+            if calculateAge(currInd, deathDate) >= 150:
+                retList += [("Error US07: " + currInd['NAME'] + "was too old when they passed away.")]
         else:
-            age = currDate - currInd['BIRT']
+            age = calculateAge(currInd, currDate)
             if age >= 150:
-                retList += [("US07: " + currInd['NAME'] + "is too old.")]
+                retList += [("Error US07: " + currInd['NAME'] + "is too old.")]
     return retList
 
